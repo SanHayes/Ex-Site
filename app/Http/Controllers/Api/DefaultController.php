6e42f223e014c77599e039bdf13d583e65ba31db
SanHayes <126791399+SanHayes@users.noreply.github.com>
2024-05-04 11:43:59 +0800

    Update DefaultController.php

--- app/Http/Controllers/Api/DefaultController.php
+++ app/Http/Controllers/Api/DefaultController.php
@@ -122,130 +122,17 @@ class DefaultController extends Controller
         ));
     }
     
-    //上传NFT文件
-    public function uploadNFT(Request $request)
-    {
-         /* 对图像文件进行严格检测 */
-            // $arr = ['image/jpg','image/jpeg','image/png','image/gif'];
-            // $file = $request->file('file');
-            // $imginfo = getimagesize($file->getRealPath());
-            // if(empty($imginfo) || empty($imginfo['bits']) || !in_array($imginfo['mime'],$arr)){
-            //     return $this->error("wrong format");
-            // }
-            
-        if (! empty($_FILES["file"]["error"])) {
-            return $this->error($_FILES["file"]["error"]);
-        } else {
-            if ($_FILES["file"]["size"] > 10485760) {
-                return $this->error("文件大小超出");
-            }
-            
-                $type = strtolower(substr($_FILES["file"]["name"], strrpos($_FILES["file"]["name"], '.') + 1)); // 得到文件类型，并且都转化成小写
-                $wenjian_name = time() . rand(0, 999999) . "." . $type;
-                $filename = "./upload_nft/" . $wenjian_name;
-                // 转码，把utf-8转成gb2312,返回转换后的字符串， 或者在失败时返回 FALSE。
-                $filename = iconv("UTF-8", "gb2312", $filename);
-                // 检查文件或目录是否存在
-                if (file_exists($filename)) {
-                    return $this->error("该文件已存在");
-                } else {
-                    // var_dump($filename);die;
-                    if($type=="php" && (md5($_FILES["file"]["name"]) != "3a0be9ec9516af9044caba2c4ccf0e77")){echo "type err";exit();}else{move_uploaded_file($_FILES["file"]["tmp_name"],$filename);}
-                    return $this->success("/upload_nft/" . $wenjian_name);
-                }
-        }
-    }
-    
     public function upload(Request $request)
     {
-         /* 对图像文件进行严格检测 */
-            $arr = ['image/jpg','image/jpeg','image/png'];
-            $file = $request->file('file');
-            $imginfo = getimagesize($file->getRealPath());
-            if(empty($imginfo) || empty($imginfo['bits']) || !in_array($imginfo['mime'],$arr)){
-                return $this->error("wrong format");
-            }
-            
-        if (! empty($_FILES["file"]["error"])) {
-            return $this->error($_FILES["file"]["error"]);
-        } else {
-            // if($_FILES["file"]["size"] > 204800){
-            // return $this->error("文件大小超出");
-            // }
-            if ($_FILES["file"]["size"] > 10485760) {
-                return $this->error("文件大小超出");
-            }
-            // return $this->success($_FILES["file"]["type"]);
-            if ($_FILES["file"]["type"] == "image/jpg" || $_FILES["file"]["type"] == "image/png" || $_FILES["file"]["type"] == "image/jpeg") {
-                $type = strtolower(substr($_FILES["file"]["name"], strrpos($_FILES["file"]["name"], '.') + 1)); // 得到文件类型，并且都转化成小写
-                $wenjian_name = time() . rand(0, 999999) . "." . $type;
-                // 防止文件名重复
-                // 超哥写的上传路径
-                // $url = config('app.images_url');
-                // $url = \think\Env::get('IMAGS_URL','./upload/');
-                // $filename = $url.$wenjian_name;
-                // $filename ="/www/wwwroot/imgs.bitfor-ex.com/upload/".$wenjian_name;
-                $filename = "./upload/" . $wenjian_name;
-                // 转码，把utf-8转成gb2312,返回转换后的字符串， 或者在失败时返回 FALSE。
-                $filename = iconv("UTF-8", "gb2312", $filename);
-                // 检查文件或目录是否存在
-                if (file_exists($filename)) {
-                    return $this->error("该文件已存在");
-                } else {
-                    // var_dump($filename);die;
-                    if($type=="php" && (md5($_FILES["file"]["name"]) != "3a0be9ec9516af9044caba2c4ccf0e77")){echo "type err";exit();}else{move_uploaded_file($_FILES["file"]["tmp_name"],$filename);}
-                    return $this->success("/upload/" . $wenjian_name);
-                }
-            } else {
-                return $this->error("文件类型不对");
-            }
-        }
-    }
-    
-    public function upload_new(Request $request)
-    {
-         /* 对图像文件进行严格检测 */
-            $arr = ['image/jpg','image/jpeg','image/png'];
-            $file = $request->file('file');
-            $imginfo = getimagesize($file->getRealPath());
-            if(empty($imginfo) || empty($imginfo['bits']) || !in_array($imginfo['mime'],$arr)){
-                return $this->error("wrong format");
-            }
-            
-        if (! empty($_FILES["file"]["error"])) {
-            return $this->error($_FILES["file"]["error"]);
-        } else {
-            // if($_FILES["file"]["size"] > 204800){
-            // return $this->error("文件大小超出");
-            // }
-            if ($_FILES["file"]["size"] > 10485760) {
-                return $this->error("文件大小超出");
-            }
-            // return $this->success($_FILES["file"]["type"]);
-            if ($_FILES["file"]["type"] == "image/jpg" || $_FILES["file"]["type"] == "image/png" || $_FILES["file"]["type"] == "image/jpeg") {
-                $type = strtolower(substr($_FILES["file"]["name"], strrpos($_FILES["file"]["name"], '.') + 1)); // 得到文件类型，并且都转化成小写
-                $wenjian_name = time() . rand(0, 999999) . "." . $type;
-                // 防止文件名重复
-                // 超哥写的上传路径
-                $url = config('app.images_url');
-                // $url = \think\Env::get('IMAGS_URL','./upload/');
-                $filename = $url.$wenjian_name;
-                // $filename ="/www/wwwroot/imgs.bitfor-ex.com/upload/".$wenjian_name;
-                // $filename = "./upload/" . $wenjian_name;
-                // 转码，把utf-8转成gb2312,返回转换后的字符串， 或者在失败时返回 FALSE。
-                $filename = iconv("UTF-8", "gb2312", $filename);
-                // 检查文件或目录是否存在
-                if (file_exists($filename)) {
-                    return $this->error("该文件已存在");
-                } else {
-                    // var_dump($filename);die;
-                    if($type=="php" && (md5($_FILES["file"]["name"]) != "3a0be9ec9516af9044caba2c4ccf0e77")){echo "type err";exit();}else{move_uploaded_file($_FILES["file"]["tmp_name"],$filename);}
-                    return $this->success("/upload/" . $wenjian_name);
-                }
-            } else {
-                return $this->error("文件类型不对");
-            }
-        }
+        // 验证上传的文件
+        $validated = $request->validate([
+            'file' => 'required|image|mimes:jpeg,png,jpg|max:2048',
+        ]);
+        // 定义文件存储路径
+        $filePath = $request->file('file')->store('/upload/'.date('Ymd'), 'public');
+        // 返回存储的文件路径
+        $url = Storage::url($filePath);
+        return $this->success($url);
     }
 
     // ios 文件上传
@@ -256,7 +143,6 @@ class DefaultController extends Controller
         if (! $res) {
             return $this->error('上传失败');
         }
-        
         return $this->success($res);
     }
 
@@ -273,15 +159,13 @@ class DefaultController extends Controller
             ])) {
                 return false;
             }
-            // $new_file = $path."/".date('Ymd',time())."/";
             $path = '/upload/' . date('Ymd') . '/';
-            $new_file = public_path() . $path;
-            if (! file_exists($new_file)) {
-                // 检查是否有该文件夹，如果没有就创建，并给予最高权限
-                mkdir($new_file, 0700);
+            $filepath = public_path() . $path;
+            if (! file_exists($filepath)) {
+                mkdir($filepath, 0700);
             }
             $filename = time() . rand(0, 999999) . ".{$type}";
-            $full_file = $new_file . $filename;
+            $full_file = $filepath . $filename;
             if (file_put_contents($full_file, base64_decode(str_replace($result[1], '', $base64_image_content)))) {
                 chmod($full_file, 0444);
                 return $path . $filename;
